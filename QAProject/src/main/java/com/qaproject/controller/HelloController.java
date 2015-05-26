5ab98fc9865e88abc33039fe5588f70f80c5c990
byonls <byonls.hcm@gmail.com>
2015-05-26 16:43:12 +0700

    update controller/HelloController.java

--- QAProject/src/main/java/com/qaproject/controller/HelloController.java
+++ QAProject/src/main/java/com/qaproject/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(value = "/",method = RequestMethod.GET)
      public String printWelcome(ModelMap model) {
-        return "newsfeed";
+        return "welcome";
     }
     @RequestMapping(value = "/homepage",method = RequestMethod.GET)
     public String homepage(ModelMap model) {
@@ -18,5 +18,29 @@ public class HelloController {
     @RequestMapping(value = "/classroom",method = RequestMethod.GET)
     public String classroom(ModelMap model) {
         return "classroom";
+    }
+    @RequestMapping(value = "/question",method = RequestMethod.GET)
+    public String question(ModelMap model) {
+        return "question";
+    }
+    @RequestMapping(value = "/article",method = RequestMethod.GET)
+    public String article(ModelMap model) {
+        return "article";
+    }
+    @RequestMapping(value = "/newsfeed",method = RequestMethod.GET)
+    public String newsfeed(ModelMap model) {
+        return "newsfeed";
+    }
+    @RequestMapping(value = "/studentdashboard",method = RequestMethod.GET)
+    public String studentdashboard(ModelMap model) {
+        return "studentdashboard";
+    }
+    @RequestMapping(value = "/teacherdashboard",method = RequestMethod.GET)
+    public String teacherdashboard(ModelMap model) {
+        return "teacherdashboard";
+    }
+    @RequestMapping(value = "/profile",method = RequestMethod.GET)
+    public String profile(ModelMap model) {
+        return "profile";
     }
 }
