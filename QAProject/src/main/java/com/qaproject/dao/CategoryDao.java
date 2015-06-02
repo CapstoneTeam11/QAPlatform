8cc4ace2cf805451c2ebac89cae8efc6af86b977
sicacho <trustme013@gmail.com>
2015-06-02 11:12:56 +0700

    update dao/CategoryDao.java

--- QAProject/src/main/java/com/qaproject/dao/CategoryDao.java
+++ QAProject/src/main/java/com/qaproject/dao/CategoryDao.java
@@ -1,10 +1,23 @@
 package com.qaproject.dao;
 
+import com.qaproject.entity.Category;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by khangtnse60992 on 5/30/2015.
  */
 @Repository
 public interface CategoryDao {
+    public Category find(Integer id);
+    List<Category> findAll();
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void persist(Category entity);
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void merge(Category entity);
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void remove(Category entity);
 }
