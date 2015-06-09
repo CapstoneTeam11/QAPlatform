a440da1a459cbc1a106c7bf8d3670338399c0d3e
byonls <byonls.hcm@gmail.com>
2015-06-09 09:05:11 +0700

    model.addAttribute("folderName",folder.getName()); in function public String material(@PathVariable Integer id,                            ModelMap model)

--- QAProject/src/main/java/com/qaproject/controller/MaterialController.java
+++ QAProject/src/main/java/com/qaproject/controller/MaterialController.java
@@ -67,6 +67,7 @@ public class MaterialController {
             return "redirect:/";
         }
         List<Material> materials = folder.getMaterialList();
+        model.addAttribute("folderName",folder.getName());
         model.addAttribute("materials", materials);
         return "materialList";
     }
