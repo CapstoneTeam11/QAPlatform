aa4b0ffd38070c974c6e321bfb799f3aa118ae5e
byonls <byonls.hcm@gmail.com>
2015-06-10 16:28:08 +0700

    implement get list request of student

--- QAProject/src/main/java/com/qaproject/dao/impl/ClassroomUserDaoImpl.java
+++ QAProject/src/main/java/com/qaproject/dao/impl/ClassroomUserDaoImpl.java
@@ -2,12 +2,30 @@ package com.qaproject.dao.impl;
 
 import com.qaproject.dao.BaseDao;
 import com.qaproject.dao.ClassroomUserDao;
+import com.qaproject.entity.Classroom;
 import com.qaproject.entity.ClassroomUser;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Created by khangtnse60992 on 5/30/2015.
  */
 @Repository
 public class ClassroomUserDaoImpl extends BaseDao<ClassroomUser,Integer> implements ClassroomUserDao{
+    @Override
+    public List<ClassroomUser> findByTypeAndClassroom(Integer type, Classroom classroom) {
+        List<ClassroomUser> classroomUsers = null;
+        Query query = entityManager.createQuery("Select cu from ClassroomUser cu where cu.type=:type and " +
+                "cu.classroomId=:classroom ");
+        query.setParameter("type",type);
+        query.setParameter("classroom",classroom);
+        try {
+            classroomUsers = query.getResultList();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return classroomUsers;
+    }
 }
