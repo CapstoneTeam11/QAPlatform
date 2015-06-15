7c2d7bc03e012684be5c69897956404539c41782
byonls <byonls.hcm@gmail.com>
2015-06-15 10:00:58 +0700

    add Scheduler

---
+++ QAProject/src/main/java/com/qaproject/util/Scheduler.java
@@ -0,0 +1,24 @@
+package com.qaproject.util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Minh on 6/14/2015.
+ */
+@Component("scheduler")
+public class Scheduler {
+
+    @Autowired
+    NewsFeedUtilities newsFeedUtilities;
+
+    public void updateRedisDB() {
+        System.out.println("Start scheduler");
+        newsFeedUtilities.setNewsFeedQuestion();
+        System.out.println("End scheduler");
+    }
+}
+
