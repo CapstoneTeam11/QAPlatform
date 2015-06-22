55748b259a298c0d283eacabcf801f259284edcf
byonls <byonls.hcm@gmail.com>
2015-06-22 18:55:21 +0700

    add PostInvitationDto

---
+++ QAProject/src/main/java/com/qaproject/dto/PostInvitationDto.java
@@ -0,0 +1,70 @@
+package com.qaproject.dto;
+
+/**
+ * Created by Minh on 6/22/2015.
+ */
+public class PostInvitationDto {
+    private Integer id;
+    private Integer postId;
+    private String postTitle;
+    private Integer teacherId;
+    private String teacherName;
+    private Integer classroomId;
+    private String classroomName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public Integer getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(Integer classroomId) {
+        this.classroomId = classroomId;
+    }
+
+    public String getClassroomName() {
+        return classroomName;
+    }
+
+    public void setClassroomName(String classroomName) {
+        this.classroomName = classroomName;
+    }
+}
