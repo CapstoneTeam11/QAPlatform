3545bef402b38529712ef0b7d8e986a654687808
byonls <byonls.hcm@gmail.com>
2015-06-22 18:54:38 +0700

    add ClassroomInvitationDto

---
+++ QAProject/src/main/java/com/qaproject/dto/ClassroomInvitationDto.java
@@ -0,0 +1,43 @@
+package com.qaproject.dto;
+
+/**
+ * Created by Minh on 6/22/2015.
+ */
+public class ClassroomInvitationDto {
+    private Integer classroomId;
+    private String classroomName;
+    private Integer teacherId;
+    private String teacherName;
+
+    public Integer getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(Integer classroomId) {
+        this.classroomId = classroomId;
+    }
+
+    public String getClassroomName() {
+        return classroomName;
+    }
+
+    public void setClassroomName(String classroomName) {
+        this.classroomName = classroomName;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+}
