1edf300ba10f06fe543449b4f734e956098e5606
byonls <byonls.hcm@gmail.com>
2015-06-24 13:09:17 +0700

    NotificationDto

---
+++ QAProject/src/main/java/com/qaproject/dto/NotificationDto.java
@@ -0,0 +1,88 @@
+package com.qaproject.dto;
+
+/**
+ * Created by Minh on 6/24/2015.
+ */
+public class NotificationDto {
+    private Integer id;
+    private Integer receiverId;
+    private Integer senderId;
+    private String senderDisplayName;
+    private Integer objectId;
+    private String objectName;
+    private Integer notificationType;
+    private Integer isView;
+    private String href;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public Integer getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getSenderDisplayName() {
+        return senderDisplayName;
+    }
+
+    public void setSenderDisplayName(String senderDisplayName) {
+        this.senderDisplayName = senderDisplayName;
+    }
+
+    public Integer getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(Integer objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public Integer getNotificationType() {
+        return notificationType;
+    }
+
+    public void setNotificationType(Integer notificationType) {
+        this.notificationType = notificationType;
+    }
+
+    public Integer getIsView() {
+        return isView;
+    }
+
+    public void setIsView(Integer isView) {
+        this.isView = isView;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+}
