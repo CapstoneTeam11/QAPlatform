f184b06441ec4799804f4f3b731c3583b0ac1b03
byonls <byonls.hcm@gmail.com>
2015-06-28 17:55:48 +0700

    add RequestDto

---
+++ QAProject/src/main/java/com/qaproject/dto/RequestDto.java
@@ -0,0 +1,43 @@
+package com.qaproject.dto;
+
+/**
+ * Created by Minh on 6/28/2015.
+ */
+public class RequestDto {
+    private Integer id;
+    private Integer studentId;
+    private String studentName;
+    private String studentProfileImageURL;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentProfileImageURL() {
+        return studentProfileImageURL;
+    }
+
+    public void setStudentProfileImageURL(String studentProfileImageURL) {
+        this.studentProfileImageURL = studentProfileImageURL;
+    }
+}
